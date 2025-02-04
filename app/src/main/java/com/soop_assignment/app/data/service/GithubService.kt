475398ecb78260075633b2cb80b7b1ecac4fadf8
이용인 <yongin0517@gmail.com>
2025-02-05 02:13:09 +0900

    Feat: 깃헙 API 요청 메소드 정의

---
+++ app/src/main/java/com/soop_assignment/app/data/service/GithubService.kt
@@ -0,0 +1,32 @@
+package com.soop_assignment.app.data.service
+
+import com.skydoves.sandwich.ApiResponse
+import com.soop_assignment.app.data.model.GitHubOwner
+import com.soop_assignment.app.data.model.GitHubRepo
+import com.soop_assignment.app.data.model.GitHubSearchResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface GitHubApiService {
+    @GET("search/repositories")
+    suspend fun searchRepositories(
+        @Query("q") query: String
+    ): ApiResponse<GitHubSearchResponse>
+
+    @GET("repos/{owner}/{repo}")
+    suspend fun getRepository(
+        @Path("owner") owner: String,
+        @Path("repo") repo: String
+    ): ApiResponse<GitHubRepo>
+
+    @GET("users/{username}/repos")
+    suspend fun getUserRepositories(
+        @Path("username") username: String
+    ): ApiResponse<List<GitHubRepo>>
+
+    @GET("users/{username}")
+    suspend fun getUserInfo(
+        @Path("username") username: String
+    ): ApiResponse<GitHubOwner>
+}
