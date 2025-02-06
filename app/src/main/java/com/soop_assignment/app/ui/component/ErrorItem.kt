e007d7a38d86854de1bc226ffd465b1aba0103f3
이용인 <yongin0517@gmail.com>
2025-02-06 21:12:16 +0900

    Feat: 에러 컴포넌트 생성

---
+++ app/src/main/java/com/soop_assignment/app/ui/component/ErrorItem.kt
@@ -0,0 +1,25 @@
+package com.soop_assignment.app.ui.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.soop_assignment.app.ui.theme.Typography
+
+@Composable
+fun ErrorMessage(code: Int, message: String) {
+    Column {
+        Text("${code}", style = Typography.titleLarge, color = Color.Gray, modifier = Modifier.padding(bottom = 20.dp))
+        Text("${message}:(", style = Typography.bodyLarge, color = Color.Gray)
+    }
+}
+
+@Preview
+@Composable
+fun ErrorPreview() {
+    ErrorMessage(code = 503, message = "Service unavailable")
+}
