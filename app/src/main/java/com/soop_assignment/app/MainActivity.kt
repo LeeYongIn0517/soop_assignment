6df946c9d8adeada4c7f419251642d4b6c65ab59
이용인 <yongin0517@gmail.com>
2025-02-08 00:18:53 +0900

    Delete: 불필요 코드삭제

--- app/src/main/java/com/soop_assignment/app/MainActivity.kt
+++ app/src/main/java/com/soop_assignment/app/MainActivity.kt
@@ -4,14 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.soop_assignment.app.presentation.navigation.Route
 import com.soop_assignment.app.presentation.navigation.SetNavGraph
-import com.soop_assignment.app.ui.theme.Soop_assignmentTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -27,21 +22,5 @@ class MainActivity : ComponentActivity() {
                 startDestination = Route.SearchRepository.name
             )
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Soop_assignmentTheme {
-        Greeting("Android")
     }
 }
